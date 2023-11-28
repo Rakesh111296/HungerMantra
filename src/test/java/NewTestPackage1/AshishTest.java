48238357fbfbd2dcc7d66547844969c6f2858631
LENOVO <LENOVO@DESKTOP-FNAOHNH>
2023-11-28 15:38:25 +0530

    dhkj

--- src/test/java/NewTestPackage1/AshishTest.java
+++ src/test/java/NewTestPackage1/AshishTest.java
@@ -2,7 +2,7 @@ package NewTestPackage1;
 
 public class AshishTest {
 	public static void main(String[] args) {
-		
+		int a=10;
 		System.out.println("hello");
 		
 		System.out.println("hiiiiiiiiii");
@@ -11,7 +11,7 @@ public class AshishTest {
 		fm.a=1000;
 		System.out.println(fm.a);
 
-
+a=a*100;
 
 		System.out.println("Hi From Rakesh");
     
